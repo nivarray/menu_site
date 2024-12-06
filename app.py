74e14bb1df983704f8463488739b0bec026e76e1
Raymond Nivar <nivarraymond@gmail.com>
2024-12-06 13:46:30 -0500

    Added actual pictures of cookies and updated the list of cookies for the menu website, and improved overall look of the webpage.

--- app.py
+++ app.py
@@ -10,20 +10,60 @@ menu_items = [
         'name': 'Chocolate Chip Cookie',
         'description': 'Classic chocolate chip cookie with gooey chocolate chips.',
         'price': '$2.50',
-        'image_url': 'static/images/kitten.png' 
+        'image_url': 'static/images/Chocolate_Chip.png', 
+        'Availability':'In Stock!'
     },
     {
-        'name': 'Oatmeal Raisin Cookie',
+        'name': 'M&M’s Cookie',
         'description': 'Hearty oatmeal cookie with plump raisins.',
         'price': '$2.00',
-        'image_url': 'static/images/capy.jpg'
+        'image_url': 'static/images/Biscoff_Butter.PNG', 
+        'Availability':'In Stock!'
     },
     {
-        'name': 'Butter Pecan Cookie',
+        'name': 'S’mores Cookie',
         'description': 'A buttery, soft cookie loaded with roasted pecans and a hint of vanilla, offering a delightful crunch in every bite.',
         'price': '$2.00',
-        'image_url': 'static/images/angry_gunter.png'
-    }]
+        'image_url': 'static/images/Smores.png', 
+        'Availability':'In Stock!'
+    },
+    {
+        'name': 'Fruity Pebbles Cookie',
+        'description': 'A buttery, soft cookie loaded with roasted pecans and a hint of vanilla, offering a delightful crunch in every bite.',
+        'price': '$2.00',
+        'image_url': 'static/images/Fruity_Pebbles.png', 
+        'Availability':'In Stock!'
+    },
+    {
+        'name': 'Cranberry Oatmeal Cookie',
+        'description': 'A buttery, soft cookie loaded with roasted pecans and a hint of vanilla, offering a delightful crunch in every bite.',
+        'price': '$2.00',
+        'image_url': 'static/images/Oatmeal_Cranberry.png', 
+        'Availability':'In Stock!'
+    },
+    {
+        'name': 'White Chocolate Macadamia Cookie',
+        'description': 'A buttery, soft cookie loaded with roasted pecans and a hint of vanilla, offering a delightful crunch in every bite.',
+        'price': '$2.00',
+        'image_url': 'static/images/White_Chocolate_Macadamia.png', 
+        'Availability':'In Stock!'
+    },
+    {
+        'name': 'Pumpkin Snickerdoodle Cookie',
+        'description': 'A buttery, soft cookie loaded with roasted pecans and a hint of vanilla, offering a delightful crunch in every bite.',
+        'price': '$2.00',
+        'image_url': 'static/images/angry_gunter.png', 
+        'Availability':'In Stock!'
+    },
+    {
+        'name': 'Apple Pie Cookie',
+        'description': 'A buttery, soft cookie loaded with roasted pecans and a hint of vanilla, offering a delightful crunch in every bite.',
+        'price': '$2.00',
+        'image_url': 'static/images/angry_gunter.png', 
+        'Availability':'In Stock!'
+    },
+    ]
+
 
 # main page is menu page
 @app.route('/')
