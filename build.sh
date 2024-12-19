977efdd0f5a67ddf08fe4de35e84a1a9bd4e55bb
Raymond Nivar <nivarraymond@gmail.com>
2024-12-19 16:20:51 -0500

    Modified build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,12 @@ apt-get install -y \
 	pkg-config \
 	meson \
 	build-essential \
-	python3-dev
-pip install -r requirements.txt
+	python3-dev \
+	libglib2.0-dev \
+	python3-gi \
+	python3-gi-cairo \
+	libcairo2-dev \
+	libpango1.0-dev \
+	libffi-dev \
+	zlib1g-dev \
+&& pip install -r requirements.txt
