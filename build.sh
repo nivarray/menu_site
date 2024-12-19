db2ce8d036c770dc439e6ebeb6e5f4c1474c6d50
Raymond Nivar <nivarraymond@gmail.com>
2024-12-19 15:00:20 -0500

    Modified build.sh and render.yaml

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 apt-get update
-apt-get install -y libgirepository1.0-dev gobject-introspection
+apt-get install -y \
+	libgirepository1.0-dev \
+	gobject-introspection \
+	pkg-config \
+	meson \
+	build-essential \
+	python3-dev
 pip install -r requirements.txt
